996e2d338e67fc6b712f1adcb850c2c4210d38d1
WillRD <willianrodriguesdos1@gmail.com>
2024-09-21 21:08:53 -0300

    Iniciando projeto de uma Landing Page

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
+  server: {
+    port: 4000,
+  },
   output: "server",
   adapter: vercel()
 });
